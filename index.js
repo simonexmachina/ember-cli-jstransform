f87d03cce8935ef2a0ecd1b767ca7f939b287327
Simon Wade <simon.wade@gmail.com>
2014-09-23 16:23:23 +1000

    Remove debug output

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var jstransform = require('broccoli-jstransform');
 module.exports = {
   name: 'ember-cli-jstransform',
   included: function(app) {
-    console.log('ember-cli-jstransform included');
     this._super.included.apply(this, arguments);
 
     app.registry.add('js', {
