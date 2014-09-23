aacbf078fee3d22f6e40b032041ee90427b4d5b1
Simon Wade <simon.wade@gmail.com>
2014-09-23 10:24:32 +1000

    Updated to use newer ember-cli addon integration

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-var JSTransform = require('broccoli-jstransform');
+'use strict';
 
-function JSTransformPlugin(options) {
-  this.name = 'ember-cli-jstransform';
-  this.ext = 'js';
-  this.options = options || {};
-}
+var jstransform = require('broccoli-jstransform');
 
-JSTransformPlugin.prototype.toTree = function(tree) {
-  return new JSTransform(tree, this.options);
+module.exports = {
+  name: 'ember-cli-jstransform',
+  included: function(app) {
+    console.log('ember-cli-jstransform included');
+    this._super.included.apply(this, arguments);
+
+    app.registry.add('js', {
+      name: 'ember-cli-jstransform',
+      ext: 'js',
+      toTree: function(tree) {
+        return jstransform(tree, app.options.jstransformOptions);
+      }
+    });
+  }
 };
-
-function EmberCLIJSTransform(project) {
-  this.project = project;
-  this.name    = 'Ember CLI JSTransform';
-}
-
-EmberCLIJSTransform.prototype.treeFor = function treeFor() {
-};
-
-EmberCLIJSTransform.prototype.included = function included(app) {
-  this.app = app;
-  app.registry.add('js', new JSTransformPlugin(this.app.options.jstransformOptions));
-};
-
-module.exports = EmberCLIJSTransform;
